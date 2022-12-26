9bd8c99b318717474ffa0c7c8c304814fbcd48e5
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2022-12-25 19:43:46 -0500

    implement parameterized rout

--- server.js
+++ server.js
@@ -6,12 +6,43 @@ const app = express()
 // set the port for the app to listen on
 const PORT = 3000
 
-// handle routes by using request handler methods
-app.get('/', (req, res) => {
-    res.send({
-        id: 1,
+const friends = [
+    {
+        id: 0,
         name: "Sir Isaac Newton"
-    })
+    },
+    {
+        id: 1,
+        name: "Sir Alberto Gonzalez"
+    },
+    {
+        id: 2,
+        name: "Sir Adriano Fazio"
+    },
+    {
+        id: 3,
+        name: "Sir Leonardo Dorado"
+    }
+]
+
+// handle routes by using request handler methods
+app.get('/friends', (req, res) => {
+    res.json(friends)
+})
+    // parameterized route with parameter => id
+app.get('/friends/:id', (req, res) => {
+    // extract the id comming from the request 
+    const friendId = Number(req.params.id)
+    // use the friendId to access the specific friend
+    const friend = friends[friendId]
+    // validate if friend exists
+    if (friend) {
+        res.json(friend)
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        })
+    }
 })
 
 app.get('/messages', (req, res) => {
