5dac23a582f63285666115664959aadee772c469
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2022-12-26 13:56:39 -0500

    serve page as express template

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -3,7 +3,11 @@ const path = require('path')
 function getMessages(req, res) {
     // sendFiles handles data that is not text or json
     // sendfiles requires the absolute path of the file. path allows us to get just that 
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    res.render('messages', {
+        title: 'Messages to my friends!',
+        friend: 'Peter Griffin',
+    })
 }
 
 function postMessage(req, res) {
